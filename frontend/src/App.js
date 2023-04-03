b6ddd9b41f754649e008f099dc45cc18808486ca
fgacek@student.agh.edu.pl <fgacek@student.agh.edu.pl>
2023-04-03 10:32:22 +0200

    deploy test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,42 @@
-import './App.css';
+import "./App.css";
+// import BPMNjs from "bpmn-js";
+import React from 'react';
+import {useEffect, useRef} from 'react';
 
-function App() {
+
+import ReactBpmn from 'react-bpmn';
+
+
+function App(props) {
+
+  function onShown() {
+    console.log('diagram shown');
+  }
+
+  function onLoading() {
+    console.log('diagram loading');
+  }
+
+  function onError(err) {
+    console.log('failed to show diagram');
+  }
+
   return (
-    <div className="App">
-        DUPA
+    <div style={{height: '70vh', width: '100vw'}}>
+      <ReactBpmn
+            url="gpt_files/proces.bpmn"
+            onShown={ onShown }
+            onLoading={ onLoading }
+            onError={ onError }
+            
+            // style={{height: '50vh'}}
+          />
+        <div style={{height: '10vh'}}>
+        DEPLOY TEST HERE
+
+        </div>
     </div>
+    
   );
 }
 
